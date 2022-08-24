89ebd31a58293b96694f85dd75fdc0e705f875f3
Hercules <hercules.sousa@academico.ifpb.edu.br>
2022-08-24 15:13:01 -0300

    Removendo anotações desnecessárias

--- backend/src/main/java/br/edu/ifpb/backend/application/controller/StudentController.java
+++ backend/src/main/java/br/edu/ifpb/backend/application/controller/StudentController.java
@@ -1,18 +1,16 @@
 package br.edu.ifpb.backend.application.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/api/v1/student")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class StudentController {
     @GetMapping
-    @ResponseStatus(HttpStatus.FOUND)
-    public String index() {
+    public String index(){
         System.out.println("Testando rota");
-        return "Test";
+        return "Okay";
     }
 }
